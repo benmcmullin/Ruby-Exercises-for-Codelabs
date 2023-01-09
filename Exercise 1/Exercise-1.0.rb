e8deed0bfd8c2b1b582f2ae1584ed6c3925bdcb3
Ben McMullin <46494991+benmcmullin@users.noreply.github.com>
2023-01-09 15:04:59 -0600

    Initial Commit

---
+++ Exercise 1/Exercise-1.0.rb
@@ -0,0 +1,37 @@
+my_array= [0,1,2,4563,23,8,74,5]
+distance = 30
+time = 10
+speed = distance / time
+cookies = 15
+cookieDozen = cookies <= 12 ? "You have a dozen(12) cookies. " : "You have more than a dozen(12) cookies."
+age = 22
+puts "Hello, World"
+puts 1
+puts true
+puts my_hash = {
+    "Hello" => "Hola",
+    "Ben's favorite number: " => 17,
+    "Array of Numbers" => [0,1,2,3]}
+puts "This is the array - #{my_array}"
+puts :testSymbol
+puts 1 + 1
+puts 2 - 1
+puts 10 / 2
+puts 2 * 5
+puts 3 ** 4
+puts 8 % 2
+puts "This is " + "an example of " + "string concatenation"
+print "abcdefghijklmnopqrstuvwxyz"[7]
+puts "\nThis is the answer to average speed question - #{speed}"
+puts "Ternary Operator: #{cookieDozen}"
+puts (1 != 2)
+puts (String == String) 
+puts (3 < 4)
+puts (4 > 6)
+puts (5 <= 5)
+puts (6 >= 7)
+puts (c= 6 + 10)
+puts ( 5 || 7)
+puts ( 3 > 2 && 4 < 7)
+puts if age < 30 
+    print "I am #{age} years old."
