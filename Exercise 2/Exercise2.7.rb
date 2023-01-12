d9591b0d23db9947f8e1c7423934e14fdd301099
Ben McMullin <46494991+benmcmullin@users.noreply.github.com>
2023-01-12 14:44:19 -0600

    Partially finished: Exercise 2.7

---
+++ Exercise 2/Exercise2.7.rb
@@ -0,0 +1,12 @@
+new_arr = [1,2,3,4,5,6,7,8,9,10]
+easy_arr = ["Hello", "World", "John"]
+puts new_arr.first()
+puts new_arr.last()
+puts new_arr.length
+puts new_arr.unshift(20)
+puts new_arr.<<(50)
+
+
+
+
+
