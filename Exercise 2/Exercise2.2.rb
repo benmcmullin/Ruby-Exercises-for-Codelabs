aa5f0b474b1edbfb02db8f083b23d4c79a9839c7
Ben McMullin <46494991+benmcmullin@users.noreply.github.com>
2023-01-12 13:14:28 -0600

    Initial Commit!

---
+++ Exercise 2/Exercise2.2.rb
@@ -0,0 +1,19 @@
+def favorite_movie(movie)
+    puts "My favorite movie is #{movie}!"
+end
+
+def favorite_food(food)
+    puts "My favorite food is #{food}!"
+end
+
+def favorite_drink(drink)
+    puts "My favorite drink is #{drink}!"
+end
+
+def list_of_favorite_things(movie, food, drink)
+    favorite_movie("The Avengers Series")
+    favorite_food("Steak")
+    favorite_drink("Sweet Tea")
+end
+
+list_of_favorite_things("Avengers",[1],[2])
