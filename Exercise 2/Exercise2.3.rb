9cb641defaa6128134ac82e881cdabad29047539
Ben McMullin <46494991+benmcmullin@users.noreply.github.com>
2023-01-12 13:21:27 -0600

    ADD: Exercise 2.3

---
+++ Exercise 2/Exercise2.3.rb
@@ -0,0 +1,5 @@
+def names_of_my_pets(name_1,name_2,name_3,name_4,name_5)
+    puts "I have 5 pets! Their names are #{name_1}, #{name_2}, #{name_3}, #{name_4}, and #{name_5}!"
+end
+
+names_of_my_pets("Nova", "Denver", "Red", "Romeo", "Austin")
