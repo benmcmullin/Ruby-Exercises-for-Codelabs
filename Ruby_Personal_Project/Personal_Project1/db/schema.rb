577a877194f47f7e42b4560e06b684ef5a50f236
Ben McMullin <46494991+benmcmullin@users.noreply.github.com>
2023-02-27 14:42:35 -0600

    ADD: Users Model

--- Ruby_Personal_Project/Personal_Project1/db/schema.rb
+++ Ruby_Personal_Project/Personal_Project1/db/schema.rb
@@ -10,11 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_20_183618) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_27_195821) do
   create_table "jobs", force: :cascade do |t|
     t.string "title"
     t.string "company"
     t.string "url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
